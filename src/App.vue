d887fa4eb101fe2e5a1f8a385390fb70a4ced783
PradipaDP <165373790+PradipaDP@users.noreply.github.com>
2024-04-05 20:40:47 +0700

    second commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,590 @@
+
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <header class="header">
+    <NavBar></NavBar>
+    </header>
+
+ <!-- Home Section -->
+    <section class="home" id="home">
+      <div class="content">
+   
+        <h2>Hi, I'm <span>Dio Pradipa</span></h2>
+        <h4>IT Student</h4>
+        <p>Student informatics engineering who are excited and always curious about the latest new technology. 
+        Having an interest in becoming an Game Developer with strong analytical skills and programming logic and can work in individually or group.</p>
+        <div class="social-icons">
+          <a href="https://www.instagram.com/dioprdpa" target="_blank"><i class="bx bxl-instagram"></i> </a>               
+          <a href="https://github.com/PradipaDP" target="_blank"><i class="bx bxl-github"></i></a>
+        </div>
+      </div>
+      <div class="gambar">
+        <img src="frontend/image/gambar1.jpeg" alt="profile" />
+      </div>
+    </section>
+
+    <!-- Biodata Section -->
+    <section class="Biodata" id="Biodata">
+      <h2 class="heading">Biodata <span>Saya</span></h2>
+      <div class="Biodata-gambar">
+        <img src="frontend/image/gambar1.jpeg" alt="">
+        <span class="circle-spin"></span>
+      </div>
+
+      <div class="Biodata-content">
+     
+        <!-- Informasi Pribadi -->
+        <div class="Biodata-item"> 
+          <h2>Informasi Pribadi</h2>
+          <ul>
+            <li><strong>Nama:</strong> Dio Pradipa Putra</li>
+            <li><strong>Usia:</strong> 20 tahun</li>
+            <li><strong>Alamat:</strong> Jl Pelita No 23B </li>
+            <li><strong>Email:</strong> diopradika01@icloud.com</li>
+            <li><strong>Telepon:</strong> 085265570730</li>
+          </ul>            
+        </div>
+        <!-- Pendidikan -->
+        <div class="Biodata-item">
+          <h2>Pendidikan</h2>
+          <ul>
+            <li><strong>SMA</strong>, SMAN 1 Pekanbaru, 2019 - 2022</li>
+            <li><strong>Teknik Informatika</strong>, Universitas Islam Riau, 2022 - sekarang</li>
+          </ul>           
+        </div>
+        <!-- Hobi -->
+        <div class="Biodata-item">
+          <h2>Hobi</h2>
+          <ul>
+            <li><strong>Ngoding</strong> </li>
+            <li><strong>Futsal</strong> </li>
+            <li><strong>Main Game</strong></li>
+          </ul>
+        </div>
+      </div>        
+    </section>
+
+
+
+
+     <!-- Galeri Section -->
+    <section class="Galeri" id="Galeri">
+      <h2 class="heading">Galeri</h2>
+      <div class="galeri">
+     
+         <div class="galeri-item">
+                <img src="frontend/image/Gallery1.jpg" alt="Gambar 1">               
+            </div>
+            <div class="galeri-item">
+                <img src="frontend/image/Gallery2.jpg" alt="Gambar 2">             
+            </div>
+            <div class="galeri-item">
+                <img src="frontend/image/Gallery3.jpg" alt="Gambar 2">             
+            </div>
+      </div>
+    </section>
+
+    <!-- Portofolio Section -->
+    <section class="Portofolio" id="Portofolio">
+      <h2 class="heading">Portofolio</h2>
+      <div class="portofolio">
+
+
+         <div class="portofolio-item2">
+                <a target= "-blank"><img src="frontend/image/Projek1.jpeg" alt="Projek 1"> </a>     
+                <h2>Project Game Zombie Shooter Menggunakan Scratch</h2>
+                <p>Deskripsi:
+                 <br>Zombie Shooter 2D adalah game aksi di mana pemain bertahan hidup dari serangan zombie. Dengan grafis 2D, pemain menggunakan berbagai senjata untuk melawan zombie dan bertahan selama mungkin.
+                    
+                 <br>Tujuan Pembuatan:
+                 <br>1. Menghibur pemain dengan pengalaman bermain yang seru.
+                 <br>2. Memenuhi permintaan pasar untuk game bertema zombie.
+                 <br>3. Menyediakan gameplay yang seru dan menantang.
+                 <br>4. Menghadirkan tantangan strategi.
+                 </p>
+            </div>
+            <div class="portofolio-item3">
+                <a target= "-blank"><img src="frontend/image/Projek2.png" alt="Projek 2"> </a>      
+                <h2>Projek Design UI/UX Menggunakan Figma</h2>
+                <p>Deskripsi:
+                <br>UI UX design aplikasi Petcare menggunakan Figma adalah tentang menciptakan antarmuka pengguna (UI) yang intuitif dan pengalaman pengguna (UX) yang memuaskan untuk aplikasi yang dirancang untuk membantu pemilik hewan peliharaan dalam merawat dan mengelola kebutuhan hewan peliharaan mereka.
+
+                <br>Tujuan Pembuatan:
+                <br>1. Memfasilitasi Pengguna: Membuat navigasi yang mudah dipahami dan tata letak yang bersih untuk penggunaan yang nyaman.
+                <br>2. Membangun Koneksi Emosional: Menggunakan elemen desain yang ramah dan menarik untuk memperkuat ikatan emosional antara pengguna dan hewan peliharaan.
+                <br>3. Meningkatkan Keterlibatan Pengguna: Menyediakan fitur-fitur yang berguna dan menarik untuk meningkatkan keterlibatan pengguna.
+                <br>4. Menyederhanakan Proses: Menyediakan fitur-fitur yang mudah diakses dan dipahami untuk menyederhanakan proses perawatan hewan peliharaan.
+                <br>5. Meningkatkan Retensi Pengguna: Menawarkan pengalaman pengguna yang memuaskan untuk meningkatkan retensi pengguna dalam menggunakan aplikasi Petcare secara konsisten.
+                </p>
+            </div>
+      </div>
+    </section>
+
+    <!-- Prestasi Section -->
+    <section class="Prestasi" id="Prestasi">
+      <h2 class="heading">Skills</h2>
+      <div class="Prestasi-content">
+
+            <div class="Prestasi-item">
+                <img src="frontend/image/Skill1.jpg" alt="">
+                <h2>Video Editing</h2>
+                <h3>Capcut, Movavi</h3>
+            </div>
+            <div class="Prestasi-item">
+                <img src="frontend/image/Skill2.jpg" alt="">
+                <h2>Graphic Design</h2>
+                <h3>Figma, Canva</h3>
+            </div>
+            <div class="Prestasi-item">
+                <img src="frontend/image/Skill3.jpg" alt="">
+                <h2>Photography</h2>
+                <h3>Dazcam, Camera</h3>
+            </div>
+      </div>
+    </section>
+
+
+ <!-- Galeri Section -->
+<section class="Galeri" id="Galeri">
+        <h2 class="heading">Galeri</h2>
+        <div class="galeri">
+    
+            <div class="galeri-item">
+                <img src="frontend/image/Gallery1.jpg" alt="Gambar 1">               
+            </div>
+            <div class="galeri-item">
+                <img src="frontend/image/Gallery2.jpg" alt="Gambar 2">             
+            </div>
+            <div class="galeri-item">
+                <img src="frontend/image/Gallery3.jpg" alt="Gambar 2">             
+            </div>
+        </div>
+    </section>
+
+  </div>
 </template>
 
+
+
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NavBar from './components/NavBar.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    NavBar
   }
 }
 </script>
 
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+/* Add your CSS styles here */
+
+/* Global styles */
+body {
+  margin: 0;
+  padding: 0;
+  font-family: Arial, sans-serif;
+  background-color: #250902; /* Set your desired background color here */
 }
+
+
+/* Scoped CSS */
+
+
+.header {
+    position: fixed;
+    top: 0;
+    left: 50%;
+    transform: translateX(-50%);
+    width: 100%;
+    padding: 20px 10%;
+    background: transparent;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    z-index: 100;
+}
+
+.navigation {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    margin: 0;
+    padding: 0;
+    list-style-type: none;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.navigation li {
+    margin-right: 40px;
+}
+
+.navigation a {
+    font-size: 1.1em;
+    color: #fbf5f5;
+    text-decoration: none;
+    margin-right: 200px; 
+    font-weight: 500;
+    transition: .3s;
+}
+
+.navigation a:hover {
+    color: #ad2831;
+}
+
+.btn-talk {
+    color: #fff;
+    text-decoration: none;
+    padding: 5px;
+    background: #ad2831;
+    border-radius: 10px;
+    border: 2px solid #ad2831;
+    font-weight: 1000;
+    transition: .3s;
+    margin-left: auto;
+}
+
+.btn-talk:hover {
+    color: #ad2831;
+    background: transparent;
+}
+
+.home {
+    position: relative;
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    padding: 30px 100px;
+}
+.content {
+    max-width: 500px;
+    color: #fff;
+}
+.content h2 {
+    font-size: 3em;
+}
+.content h2 span {
+    color: #800e13;
+}
+.content h4 {
+    font-size: 2em;
+}
+.content p {
+    padding: 10px 0 40px 0;
+}
+.btn-group a {
+    color: #fff;
+    text-decoration: none;
+    padding: 10px 40px;
+    background: #ad2831
+;
+    border-radius: 10px;
+    border: 2px solid #ad2831
+;
+    font-weight: 500;
+    transition: .3s;
+}
+.btn-group a:hover:nth-child(1) {
+    color: #ad2831
+;
+    background: transparent;
+}
+.btn-group a:nth-child(2) {
+    color: #ad2831
+;
+    background: transparent;
+    margin-left: 30px;
+    padding: 10px 30px;
+}
+.btn-group a:hover:nth-child(2) {
+    color: #fff;
+    background: #ad2831
+;
+}
+.social-icons {
+    position: absolute;
+    bottom: 50px;
+}
+.social-icons a {
+    display: inline-block;
+    width: 35px;
+    height: 35px;
+    background: #ad2831
+;
+    border-radius: 50%;
+    margin-right: 22px;
+    text-align: center;
+    line-height: 35px;
+    border: 1px solid #fff;
+    outline: 2px solid #ad2831
+;
+    transition: .3s;
+}
+.social-icons a:hover {
+    transform: translateY(-5px);
+}
+.social-icons a i {
+    color: #fff;
+}
+
+.gambar {
+  width: 70%; 
+  display: flex;
+  justify-content: center; 
+  margin-top: 1rem;
+  position: relative; 
+}
+
+.gambar img {
+  max-width: 400px; 
+  border-radius: 50%; 
+  border-top: .5rem solid #ad2831
+;
+  border-bottom: .5rem solid #ad2831
+;
+  border-left: .5rem solid #640d14;
+  border-right: .5rem solid #640d14;
+}
+
+
+
+
+
+
+
+.Biodata {
+  min-height: 100vh;
+    padding: 60px 50px;
+    display: flex;
+    flex-direction: column;
+    align-items: center; 
+  }
+  
+  .heading {
+    font-size: 2rem;
+    margin-bottom: 1rem;
+    text-align: center;
+    color: #fff; 
+  }
+  
+  span {
+    color: #ad2831
+;
+  }
+
+  .Biodata-gambar {
+    width: 70%; 
+    display: flex;
+    justify-content: center; 
+    margin-top: 1rem;
+    position: relative; 
+  }
+  
+  .Biodata-gambar img {
+    max-width: 200px; 
+    border: .3rem solid #ad2831
+;
+    border-radius: 50%; 
+  }
+  
+  .Biodata-gambar .circle-spin {
+    position: absolute; 
+    top: 50%; 
+    left: 50%; 
+    transform: translate(-50%, -50%); 
+    width: 250px; 
+    height: 275px; 
+    border-radius: 50%; 
+    border-top: .2rem solid #250902;
+    border-bottom: .2rem solid #250902;
+    border-left: .2rem solid #800e13;
+    border-right: .2rem solid #800e13;
+  }
+  
+
+  .Biodata-content {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr); 
+    gap: 20px; 
+    padding: 15px; 
+}
+
+.Biodata-item {
+    border: 2px solid #fff; 
+    padding: 20px; 
+    color: #fff; 
+    text-align: center; 
+    border: 5px solid #800e13; 
+    padding: 20px; 
+}
+
+.Biodata-item ul {
+  padding-left: 0; 
+}
+
+.Biodata-item ul li {
+  text-align: left; 
+  margin-top: 10px;
+}
+
+
+.Prestasi {
+  min-height: 100vh;
+  padding: 75px 50px;
+  display: flex;
+  flex-direction: column;
+  align-items: center; 
+}
+
+.heading {
+  font-size: 2rem;
+  margin-bottom: 1rem;
+  text-align: center;
+  color: #fff; 
+}
+
+
+
+.Prestasi-content {
+    display: flex;
+    flex-wrap: wrap;
+  }
+  
+  .Prestasi-item {
+    padding: 10px 30px 30px 30px; 
+    color: #fff; 
+    border: 5px solid #800e13; 
+    border-radius: 5%;
+    flex: 1;
+    margin: 10px;
+    text-align: center;
+  }
+  
+  .Prestasi-item img {
+   width: 250px;
+    height: auto;
+  }
+  
+
+.Galeri {
+  min-height: 100vh;
+    padding: 75px 50px;
+  }
+  
+  .heading {
+    font-size: 2rem;
+    margin-bottom: 1rem;
+    text-align: center;
+    color: #fff; 
+  }
+  
+  .galeri {
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+    gap: 25px;
+    margin-top: 1rem;
+  }
+  
+  .galeri-item {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    overflow: hidden;
+    cursor: pointer;
+    padding: 20px; 
+    color: #fff; 
+    text-align: center; 
+    border: 5px solid #800e13; 
+    border-radius: 5%;
+  }
+  
+  .galeri-item img {
+    width: 350px;
+    transition: transform 0.3s;
+  }
+
+
+
+
+
+.heading {
+  font-size: 2rem;
+  margin-bottom: 1rem;
+  text-align: center;
+  color: #fff; 
+}
+
+
+.Portofolio {
+  min-height: 100vh; 
+  padding: 75px 50px;
+}
+
+.heading {
+  font-size: 2rem;
+  margin-bottom: 1rem;
+  text-align: center;
+  color: #fff;
+}
+
+.portofolio {
+  display: grid;
+  grid-template-columns: repeat(2, minmax(250px, 1fr));
+  gap: 20px;
+}
+
+
+.portofolio-item2 {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  overflow: hidden;
+  cursor: pointer;
+  padding: 20px; 
+  color: #fff; 
+  text-align: center; 
+  border: 5px solid #800e13; 
+}
+
+.portofolio-item2 img {
+  width: 250px;
+  transition: transform 0.3s;
+}
+
+.portofolio-item2 p {
+    text-align: left;
+}
+
+.portofolio-item3 {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  overflow: hidden;
+  cursor: pointer;
+  padding: 20px; 
+  color: #fff; 
+  text-align: center; 
+  border: 5px solid #800e13; 
+}
+
+.portofolio-item3 img {
+  width: 250px;
+  transition: transform 0.3s;
+}
+
+.portofolio-item3 p {
+    text-align: left;
+}
+
+.portofolio :hover img {
+  transform: scale(1.1);
+}
+
+
 </style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,92 @@
+<template>
+     <header class="header">
+      <!-- Navigation -->
+      <nav class="navigation">
+        <a href="#home">Home</a>
+        <a href="#Biodata">Biodata</a>
+        <a href="#Portofolio">Portofolio</a>
+        <a href="#Galeri">Galeri</a>
+        <a href="#Prestasi">Prestasi</a>
+      </nav>
+      <a href="#" class="btn-talk">. . .</a>
+    </header>
+</template>
+
+<script>
+export default {
+  name: 'NavBar'
+}
+</script>
+
+
+<style scoped>
+body {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Poppins', sans-serif;
+}
+
+.header {
+    position: fixed;
+    top: 0;
+    left: 50%;
+    transform: translateX(-50%);
+    width: 100%;
+    padding: 20px 10%;
+    background: transparent;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    z-index: 100;
+}
+
+.navigation {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    margin: 0;
+    padding: 0;
+    list-style-type: none;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.navigation li {
+    margin-right: 20px; /* Reduce margin between navigation links */
+}
+
+.navigation a {
+    font-size: 1.1em;
+    color: #fbf5f5;
+    text-decoration: none;
+    font-weight: 500;
+    transition: color 0.3s; /* Apply transition to color change */
+}
+
+.navigation a:hover {
+    color: #ad2831;
+}
+
+.btn-talk {
+    color: #fff;
+    text-decoration: none;
+    padding: 5px;
+    background: #ad2831;
+    border-radius: 10px;
+    border: 2px solid #ad2831;
+    font-weight: 1000;
+    transition: background 0.3s, color 0.3s; /* Apply transition to background and color */
+    margin-left: auto;
+}
+
+.btn-talk:hover {
+    color: #ad2831;
+    background: transparent;
+}
+
+</style>
+
+
